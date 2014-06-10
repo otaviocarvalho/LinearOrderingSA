5bd6e39c523cfec5f392092be3ab538a9c0799f4
Otávio Carvalho <omcarvalho@inf.ufrgs.br>
2014-06-10 15:25:32 -0300

    Build graph and Graph cost

--- LinearOrderingSA.cpp
+++ LinearOrderingSA.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <cmath>
 
 using namespace std;
 
@@ -13,33 +14,65 @@ int main(){
     cin >> m;
     cin >> n;
 
-    vector<int> v_mapping;
-    v_mapping.reserve(m);
+    vector<int> v_degree;
+    v_degree.reserve(m);
     for (int i = 0; i < m; i++) {
         cin >> v_aux;
-        v_mapping.push_back(v_aux);
-        cout << v_mapping[i] << " ";
+        v_degree.push_back(v_aux);
+        cout << v_degree[i] << " ";
     }
     cout << endl;
 
-    vector<int> arc_mapping;
-    arc_mapping.reserve(2*n);
+    vector<int> v_values;
+    v_values.reserve(2*n);
     for (int i = 0; i < 2*n; i++) {
         cin >> v_aux;
-        arc_mapping.push_back(v_aux);
-        cout << arc_mapping[i] << " ";
+        v_values.push_back(v_aux);
+        cout << v_values[i] << " ";
     }
     cout << endl;
     cin >> v_aux; // Read -1
 
-    vector<int> func_mapping;
-    func_mapping.reserve(m);
+    vector<int> v_adj_mapping;
+    v_adj_mapping.reserve(m);
     for (int i = 0; i < m; i++) {
         cin >> v_aux;
-        func_mapping.push_back(v_aux);
-        cout << func_mapping[i] << " ";
+        v_adj_mapping.push_back(v_aux);
+        cout << v_adj_mapping[i] << " ";
     }
     cout << endl;
+
+    // Build graph
+    vector< vector<int> > graph(m);
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < v_degree[i]; j++) {
+            graph[i].push_back( v_values[ v_adj_mapping[i]+j ] );
+        }
+    }
+
+    // Print graph
+    for (int i = 0; i < graph.size(); i++) {
+        for (int j = 0; j < graph[i].size(); j++) {
+            cout << graph[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    // Calculate cost
+    int lop_sum = 0;
+    vector<int> visited(n,0);
+    for (int i = 0; i < graph.size(); i++) {
+        for (int j = 0; j < graph[i].size(); j++){
+            if ( !visited[graph[i][j]] ){
+                cout << i << " " << graph[i][j] << " " << fabs(i - graph[i][j]);
+                cout << endl;
+                lop_sum += fabs(i - graph[i][j]);
+            }
+        }
+        visited[i] = 1;
+    }
+    cout << endl << lop_sum << endl;
 
     return 0;
 }
